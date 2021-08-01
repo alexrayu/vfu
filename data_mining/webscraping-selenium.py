aca37f541600a3503606734898d828a3de3cc734
Alexei Raiu <alex.rayu@gmail.com>
2021-08-01 20:12:51 +0300

    Correct selenium. Correct the paths handling.

--- data_mining/webscraping-selenium.py
+++ data_mining/webscraping-selenium.py
@@ -1,21 +1,42 @@
 from selenium import webdriver
+import pandas as pd
 from selenium.webdriver.chrome.options import Options
+import re
+
 
 chrome_options = Options()
 chrome_options.add_experimental_option('prefs', {'profile.managed_default_content_settings.javascript': 2})
 driver = webdriver.Chrome('chromedriver', options=chrome_options)
+base_url = 'https://bazar.bg/obiavi/gradski-velosipedi/varna?condition=2'
 url = 'https://bazar.bg/obiavi/gradski-velosipedi/varna?condition=2'
-driver.get(url)
-driver.implicitly_wait(1000)
 css_selector = '.awrapper .listItemContainer .listItemLink'
-items = driver.find_elements_by_css_selector(css_selector)
-print(len(items))
-i = 0
-for item in items:
-	i += 1
-	title = item.find_element_by_css_selector('span.title').text
-	price = item.find_element_by_css_selector('span.price').text
-	if title and price:
-		print(f'{i}: {title} ({price})')
+driver.get(url)
+driver.implicitly_wait(100)
+count_pages = 5
+data = []
+for page in range(count_pages):
+    cur_page = page + 1
+    items = driver.find_elements_by_css_selector(css_selector)
+    i = 0
+    for item in items:
+        i += 1
+        title = item.find_element_by_css_selector('span.title').text
+        price = item.find_element_by_css_selector('span.price').text
+        price = re.sub('[^0-9]', '', price)
+        if not len(price):
+            price = 0
+        price = int(price)
+        image = item.find_element_by_css_selector('img.cover').get_attribute('src')
+        if title and price:
+            data.append([title, price, 'https:' + image])
+    link = driver.find_element_by_css_selector('.paging a.next')
+    if link:
+        link.click()
 
 driver.quit()
+
+
+df = pd.DataFrame(data, columns=['title', 'price', 'image'])
+df.sort_values(by='price', inplace=True)
+df.to_excel('bikes-selenium.xlsx')
+df.head()
